2a6194f7c46ae98a1401c501ec1fe93dbd740639
Eugenie Mangon <eugeniemangon@Eugenies-MacBook-Air.local>
2025-06-20 20:57:05 -0400

    Enrollment issues

--- Kambaz/Assignments/dao.js
+++ Kambaz/Assignments/dao.js
@@ -2,15 +2,6 @@
 import model from "./model.js";
 import mongoose from "mongoose";
 
-async function getNextId(courseId) {
-    const docs = await model.find({ course: courseId }).select("_id").lean();
-    const nums = docs
-        .map((d) => (d._id.match(/^A(\d+)$/) || [])[1])
-        .map((n) => parseInt(n, 10) || 0);
-    const maxNum = nums.length ? Math.max(...nums) : 100;
-    return `A${maxNum + 1}`;
-}
-
 export async function findAssignmentsForCourse(courseId) {
     const courseObjId = new mongoose.Types.ObjectId(courseId);
     return model.find({ course: courseObjId }).lean();
@@ -21,10 +12,15 @@ export async function findAssignmentById(assignmentId) {
 }
 
 export async function createAssignmentForCourse(courseId, assignment) {
-    const id = assignment._id || (await getNextId(courseId));
-    const doc = { ...assignment, _id: id, course: new mongoose.Types.ObjectId(courseId) };
+    const id = new mongoose.Types.ObjectId().toHexString();
+    const doc = {
+        ...assignment,
+        _id: id,
+        course: new mongoose.Types.ObjectId(courseId)
+    };
     return model.create(doc);
 }
+
 
 export async function updateAssignment(assignmentId, updates) {
     return model
